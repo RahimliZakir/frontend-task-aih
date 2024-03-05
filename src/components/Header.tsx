5ea4dccceb52936eb2038e9280fba86e93668aea
RahimliZakir <zakirer@code.edu.az>
2024-03-05 23:04:04 +0400

    Spinner Style Improved

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import Logo from "../assets/imgs/header/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Header = () => {
   return (
     <header>
-      <nav className="custom-container flex justify-between items-center py-5 border-b-[2px] border-solid border-[color:var(--border-color)]">
+      <nav className="custom-container bg-[color:var(--nav-color)] flex justify-between items-center py-5 border-b-[2px] border-solid border-[color:var(--border-color)]">
         <div className="flex items-center">
           <div className="w-[7.5%]">
             <img className="w-full" src={Logo} alt="Fake Store Logo" />
@@ -11,7 +13,13 @@ const Header = () => {
           <h1 className="text-2xl font-semibold ms-2">Fake Store</h1>
         </div>
         <div>
-          <a href="#">Zakir Rahimli</a>
+          <a
+            className="text-[color:var(--linkedin-icon-color)] text-[1.2rem] font-medium"
+            href="https://linkedin.com/in/rahimlizakir"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} /> Zakir Rahimli
+          </a>
         </div>
       </nav>
     </header>
